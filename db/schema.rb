3333ac534639546ecf8d12c3414fe2258f7095f5
Jeff Ginsberg <jeffginsberg87@gmail.com>
2015-09-17 13:11:10 -0400

    added migration to zombies

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150917164820) do
+ActiveRecord::Schema.define(version: 20150917170207) do
 
   create_table "zombies", force: :cascade do |t|
     t.string   "name"
     t.text     "bio"
     t.integer  "age"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.string   "email"
+    t.boolean  "rotting",    default: false
   end
 
 end
